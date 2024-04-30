f710c26ba82579da27248a16b3cc9d9b72c7d333
Jay Chotaliya <jaychotaliya@192.168.1.5>
2024-04-30 10:24:15 +0530

    requirements and Installation Done

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+with open("README.md", "r" , encoding="utf-8") as fh:
+    long_description = fh.read()
+    
+version="0.0.0"
+
+REPO_NAME="Text-Summarisation-Project"
+AUTHOR_USER_NAME="Jay15092005"
+SRC_REPO="textSummarizer"
+AUTHOR_MAIL="jaychotaliya1509@gmail.com"
